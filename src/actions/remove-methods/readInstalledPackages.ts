26f4b4c458ea8ed62c5b8e4267f232d82c469e99
Jason Rogers <jason@jmichaelrogers.com>
2017-04-01 20:21:57 -0400

    Handle case where XML parser passes null value to callback (closes #16)

--- src/actions/remove-methods/readInstalledPackages.ts
+++ src/actions/remove-methods/readInstalledPackages.ts
@@ -24,7 +24,7 @@ export default function readInstalledPackages(projFileFullPath: string): Promise
             }
 
             parseString(data, (err, parsed: any = {}) => {
-                if (err) {
+                if (err || !parsed) {
                     return handleError(
                         err,
                         getConstructedErrorMessage(
